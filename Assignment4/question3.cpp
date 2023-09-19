7f153bf595549f1d63f8d3c012a934be9d5a5c5d
VaishnaviMishra <vaishnavim424@gmail.com>
2023-09-19 14:42:09 +0530

    Assignment 7

---
+++ Assignment4/question3.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class Stack
+{private:
+    int size;
+    int index;
+    int rear;
+    int front;
+public:
+   Stack(){
+    this->size = 5;
+    int index = -1;
+    int rear = -1;
+    int front = -1;
+   }
+
+}
+
+
+
+int main(){
+Stack *s1 = new Stack[5];
+}
